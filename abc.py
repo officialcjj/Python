a28e241af4de45d29f54e58a8faf3d555229172b
officialcjj <116774757+officialcjj@users.noreply.github.com>
2023-01-28 18:18:36 -0800

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+class car(ABC):
+    def paySlip(self, amount):
+        print("Your purchase amount: ",amount)
+    @abstractmethod
+    def payment(self, amount):
+        pass
+
+class DebitCardPayment(car):
+    def payment(self, amount):
+        print('Your purchase amount of {} exceed your $100 limit'.format(amount))
+
+obj = DebitCardPayment()
+obj.paySlip("$400")
+obj.payment("$400")
